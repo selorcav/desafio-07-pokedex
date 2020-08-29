5afe9ebd89df66f02d3788b677db2c2b9c8d5d2e
sebastian <sebastian.lorca@wic.uchile.cl>
2020-08-28 20:36:18 -0400

    agregado charts

--- app.js
+++ app.js
@@ -8,8 +8,37 @@ $(document).ready(tomarNumero);{
         $.getJSON(pokeapiUrl).done(function(data){
             console.log(data);
             $('#img').attr('src', data.sprites.front_default);
-            $('#info').text(data.species.name);
+            $('#info-name').text(data.species.name);
+            
+            var chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: true,
+                theme: "light1", // "light1", "light2", "dark1", "dark2"
+                title:{
+                    text: "Stats Base"
+                },
+                axisY: {
+                    title: "Value"
+                },
+                data: [{        
+                    type: "column",  
+                    showInLegend: true, 
+                    legendMarkerColor: "grey",
+                    legendText: "Stats",
+                    dataPoints: [    
+                          
+                        { y: (data.stats[0].base_stat), label: (data.stats[0].stat.name) },
+                    ]
+                }]
+            });
+            chart.render();
         });
+
+
+
+        
+        
+
+        
     }
     
 }
