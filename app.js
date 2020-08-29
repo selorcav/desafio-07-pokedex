3b1adfca805f3b2df1920ba402b9b4c16a92fa9f
sebastian <sebastian.lorca@wic.uchile.cl>
2020-08-28 21:08:27 -0400

    agregado todas las stats

--- app.js
+++ app.js
@@ -25,10 +25,16 @@ $(document).ready(tomarNumero);{
                     legendMarkerColor: "grey",
                     legendText: "Stats",
                     dataPoints: [    
-                          
                         { y: (data.stats[0].base_stat), label: (data.stats[0].stat.name) },
+                        { y: (data.stats[1].base_stat), label: (data.stats[1].stat.name) },
+                        { y: (data.stats[2].base_stat), label: (data.stats[2].stat.name) },
+                        { y: (data.stats[3].base_stat), label: (data.stats[3].stat.name) },
+                        { y: (data.stats[4].base_stat), label: (data.stats[4].stat.name) },
+                        { y: (data.stats[5].base_stat), label: (data.stats[5].stat.name) },
                     ]
                 }]
+
+
             });
             chart.render();
         });
